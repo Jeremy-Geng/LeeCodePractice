2f5fc4883b9fd8851d4c9a08e3f71a3939154d25
Shuhao Geng <u6725016@anu.edu.au>
2020-07-25 07:16:53 +0800

    2020/07/25

--- src/Solution0602.java
+++ src/Solution0602.java
@@ -1,11 +1,231 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Solution0602 {
-    //x的平方根 2020/06/18 使用二分查找提高效率
 
+
+    //字母异位词分组 拉链法
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        List<List<String>> p = new ArrayList<>();
+        if(strs.length == 0) return p;
+
+        Map<String,List> t = new HashMap<>();
+        for(String str : strs){
+            char[] ca = str.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if(!t.containsKey(key)) t.put(key, new ArrayList());
+            t.get(key).add(str);
+        }
+
+        return new ArrayList(t.values());
+    }
+
+
+        // 时间不够快
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> p = new ArrayList<>();
+        if(strs.length == 0) return p;
+
+        List<String> first = new ArrayList<>();
+        first.add(strs[0]);
+        p.add(first);
+
+        for(int i = 1; i < strs.length;i++){
+            boolean test = false;
+
+            for(int j = 0; j < p.size();j++){
+                if(isSame(p.get(j).get(0),strs[i])) {
+                    p.get(j).add(strs[i]);
+                    test = true;
+                    break;
+                }
+            }
+
+            if(!test){
+                List<String> newOne = new ArrayList<>();
+                newOne.add(strs[i]);
+                p.add(newOne);
+            }
+        }
+
+        return p;
+    }
+
+
+    public boolean isSame(String a, String b){
+        if(a.length() != b.length()) return false;
+        HashMap<Character, Integer> p = new HashMap<>();
+        HashMap<Character, Integer> t = new HashMap<>();
+        for(int i = 0; i < a.length();i++){
+            if(p.keySet().contains(a.charAt(i))) p.put(a.charAt(i), p.get(a.charAt(i)) + 1);
+            else p.put(a.charAt(i),1);
+
+            if(t.keySet().contains(b.charAt(i))) t.put(b.charAt(i), t.get(b.charAt(i)) + 1);
+            else t.put(b.charAt(i),1);
+        }
+        return p.equals(t);
+    }
+    // 旋转图像 简单方法是traverse矩阵，再reverse每一行
+
+    public void rotate2(int[][] matrix){
+        int len = matrix.length;
+        int k = 0;
+        for(int i = k; i < len;i++){
+            for(int j = k; j < len;j++){
+                int p = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] =  p;
+            }
+
+            k++;
+        }
+
+        for(int i = 0;i < len;i++){
+            for(int j = 0;j < len/2;j++){
+                int p = matrix[i][j];
+                matrix[i][j] =  matrix[i][len-1-j];
+                matrix[i][len-1-j] = p;
+            }
+        }
+    }
+
+
+
+    public void rotate(int[][] matrix) {
+        int len = matrix.length;
+        int n = len;
+        int i = 0;
+        while(n > 1){
+            //处理四个角
+            int leftUp = matrix[i][i];
+
+            int rightUp = matrix[i][len - 1 - i];
+
+            int leftDown = matrix[len - 1 - i][i];
+
+            int rightDown = matrix[len - 1 -i][len - 1 - i];
+
+            matrix[i][i] = leftDown;
+
+            matrix[i][len - 1 - i] = leftUp;
+
+            matrix[len - 1 - i][i] = rightDown;
+
+            matrix[len - 1 -i][len - 1 - i] = rightUp;
+
+            //进行四次旋转
+
+            int up[] = new int[n-2];
+            int down[] = new int[n-2];
+            int left[] = new int[n-2];
+            int right[] = new int[n-2];
+
+
+            for(int j = 0; j < n - 2; j++){
+                up[j] = matrix[i][i + 1 + j];
+                down[j] = matrix[len - 1- i][i + 1 + j];
+                left[j] = matrix[i + 1 + j][i];
+                right[j] = matrix[i + 1 + j][len - 1 - i];
+            }
+
+
+            for(int k = 0; k < n - 2; k++){
+                //up
+                matrix[i][i+1+k] = left[n-3-k];
+
+                //down
+                matrix[len - 1- i][i + 1 + k] = right[n-3-k];
+
+                //left
+                matrix[i + 1 + k][i] = down[k];
+
+                //right
+                matrix[i + 1 + k][len - 1 - i] = up[k];
+            }
+
+            n = n - 2;
+            i++;
+
+        }
+
+    }
+
+    public void exch(int[][] matrix,int m, int n, int k, int j){
+        int p = matrix[m][n];
+        matrix[m][n] = matrix[k][j];
+        matrix[k][j] = p;
+    }
+    // 爬楼梯 2020/06/24
+    public int climbStairs(int n) {
+
+        int p[] = new int[n+1];
+        p[0] = 0;
+        p[1] = 1;
+        p[2] = 2;
+        p[3] = 3;
+        for(int i = 4; i <= n; i++){
+            p[i] = p[i-1] + p[i-1];
+        }
+        return p[n];
+    }
+
+    // 全排列II 2020/06/20
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> rst = new ArrayList<>();
+
+        if(nums.length == 0) return rst;
+        List<Integer> cands = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for(int i : nums){
+            cands.add(i);
+        }
+        backTrack(rst,cands,null);
+        return rst;
+    }
+
+    public void backTrack(List<List<Integer>> rst, List<Integer> cands, List<Integer> single){
+        if(cands.size() == 0){
+            rst.add(single);
+            return;
+        }
+
+        for(int i = 0; i < cands.size();i++){
+            if(i > 0 && cands.get(i) == cands.get(i-1) ) continue;
+            List<Integer> p = single == null? new ArrayList<>() : new ArrayList<>(single);
+            p.add(cands.get(i));
+
+            List<Integer> newCands = new ArrayList<>(cands);
+            newCands.remove(i);
+            backTrack(rst, newCands, p);
+        }
+    }
+
+
+    // 颜色分类 2020/06/20
+    public void sortColors(int[] nums) {
+       sort3waycut(nums,0,nums.length-1);
+    }
+
+    public void sort3waycut(int[] nums, int lo, int hi){
+        if(lo >= hi) return;
+        int i = lo ,lt =lo, gt = hi + 1;
+        while(i < gt){
+            if(nums[i] < 1){
+                int p = nums[lt];
+                nums[lt++] = nums[i];
+                nums[i++] = p;
+            }else if(nums[i] > 1){
+                int p = nums[i];
+                nums[i] = nums[--gt];
+                nums[gt] = p;
+            }else {
+                i++;
+            }
+        }
+    }
+
+    // x的平方根 2020/06/18 使用二分查找提高效率
     public int mySqrt(int x) {
         if(x == 0 ) return 0;
         long lo = 1, hi = x, mid = lo + (hi - lo) /2;
@@ -16,11 +236,11 @@ public class Solution0602 {
             else hi = mid-1;
             mid = lo + (hi - lo)/ 2;
         }
-
         return (int)hi;
 
     }
-    //组合总数 2020/06/15
+
+    // 组合总数 2020/06/15
     //1.已选择路径 2. 循环选择路径 3.退出条件 这三者是回溯算法的常规考虑条件
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> rst = new ArrayList<>();
@@ -161,6 +381,7 @@ public class Solution0602 {
 
     public static void main(String[] args) {
         Solution0602 s = new Solution0602();
-        System.out.println(s.mySqrt(2147395599));
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(s.groupAnagrams2(strs));
     }
 }
--- src/Solution0701.java
+++ src/Solution0701.java
@@ -1,7 +1,519 @@
 import java.util.*;
 
 public class Solution0701 {
-    //不同路径2
+    // 搜索旋转排序数组 复习
+    public int search(int[] nums, int target){
+        int n = nums.length;
+        if(n == 0) return -1;
+
+        int index = -1 , left = 0, right = n - 1, firstValue = nums[0];
+        if(target == firstValue) return 0;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2, value = nums[mid];
+            if(value == target) return mid;
+
+            if(target > firstValue){
+                if(value > target){
+                    right = mid - 1;
+                }else{
+                    if(value >= firstValue){
+                        left = mid + 1;
+                    }else{
+                        right = mid -1;
+                    }
+                }
+            }else{
+                if(value > target){
+                   if(value >=firstValue){
+                       left = mid + 1;
+                   }else{
+                       right = mid - 1;
+                   }
+                }else{
+                   left = mid + 1;
+                }
+            }
+        }
+
+        return index;
+    }
+    //搜索旋转排序数组2
+    public boolean search2(int[] nums, int target) {
+        boolean t = false;
+
+
+        return t;
+    }
+
+    //删除排序数组中的重复项2
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if(n == 0) return 0;
+
+        int count = 1;
+        int slotIndex = 1;
+        for(int i = 1; i < n;i++){
+           count = nums[i] == nums[i-1] ? count + 1 : 1;
+           if(count <= 2) nums[slotIndex++] = nums[i];
+        }
+        return slotIndex;
+    }
+
+    // 为单词搜索提供的变量
+    private boolean[][] marked;
+    private char[][] board;
+    private int m;
+    private int n;
+    private String word;
+    private int[][] directions = {{-1,0},{0,1},{1,0},{0,-1}};
+
+
+    // *单词搜索(优化)，第一次写得太乱了
+    public boolean exist2(char[][] board, String word){
+        this.board = board;
+        this.word = word;
+        m = board.length;
+        if( m == 0) return false;
+        n = board[0].length;
+        marked = new boolean[m][n];
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(dfs(i,j,0))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(int i, int j, int start){
+        if(start == word.length() -1)
+            return board[i][j] == word.charAt(start);
+
+        if(board[i][j] == word.charAt(start)){
+            marked[i][j] = true;
+            for(int k = 0; k < 4; k++){
+                int newX = i + directions[k][0];
+                int newY = j + directions[k][1];
+                if(inArea(newX,newY) && !marked[newX][newY]){
+                    if(dfs(newX,newY,start+1))
+                        return true;
+                }
+            }
+            marked[i][j] = false;
+        }
+
+        return false;
+    }
+
+    private boolean inArea(int x, int y){
+        return x >= 0 && y >=0 && x < m && y < n;
+    }
+
+
+    // *单词搜索  完成，但是时间超时，原因是剪枝不够彻底
+    public boolean exist(char[][] board, String word) {
+
+        boolean[] rst = new boolean[]{false};
+        int length = word.length();
+        char start = word.charAt(0);
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length;j++){
+                if(board[i][j] == start && !rst[0]){
+                    boolean[][] ts = new boolean[board.length][board[0].length];
+                    findString(i,j,rst,board,ts,null,word,length);
+                }
+            }
+        }
+
+        return rst[0];
+    }
+
+
+
+
+    public void findString(int m, int n, boolean[] rst, char[][] board, boolean[][] ts,String pathWord, String word, int length){
+        if(rst[0]) return;
+        if(length == 0){
+            return;
+        }
+
+        String newPathWord = "";
+        if(pathWord == null)  newPathWord = "" + board[m][n];
+        else newPathWord  = pathWord + board[m][n];
+
+
+        // 这个剪枝 必须添加，否则 会耗费大量时间
+        if(!newPathWord.equals(word.substring(0,word.length()-length+1))) return;
+
+        if(newPathWord.equals(word)) rst[0] = true;
+
+        //  防止回退
+
+        ts[m][n] = true;
+
+        // left
+        if(n -1 >= 0 && !ts[m][n-1]){
+            findString(m,n-1,rst,board, ts, newPathWord,word,length - 1);
+            ts[m][n-1] = false;
+        }
+
+
+        // right
+        if(n + 1 <= board[0].length - 1 && !ts[m][n+1])
+        {
+            findString(m,n+1,rst,board, ts, newPathWord,word,length-1);
+            ts[m][n+1] = false;
+        }
+
+
+        // up
+        if(m - 1  >= 0 && !ts[m-1][n])
+        {
+            findString(m-1, n ,rst,board, ts, newPathWord,word,length-1);
+            ts[m-1][n] =false;
+        }
+
+        // down
+        if(m + 1 <= board.length - 1 && !ts[m+1][n])
+        {
+            findString(m+1, n ,rst,board, ts, newPathWord,word,length -1);
+            ts[m+1][n] = false;
+        }
+
+    }
+
+
+    // 颜色分类（复习）
+    public void sortColors(int[] nums) {
+        int n = nums.length - 1;
+        int lt = -1, gt = n, i = 0;
+        while(i <=gt){
+            if(nums[i] > 1){
+                    int p = nums[gt];
+                    nums[gt--] = nums[i];
+                    nums[i] = p;
+            }else if(nums[i] < 1){
+                if(lt >=0){
+                    int p = nums[i];
+                    nums[i++] = nums[lt];
+                    nums[lt++] = p;
+                }else{
+                    i++;
+                }
+
+            }else{
+                if(lt < 0) {
+                    lt = i;
+                }
+                i++;
+            }
+        }
+    }
+
+
+    // 子集
+    public List<List<Integer>> subsets(int[] nums) {
+        if(nums == null) return null;
+        int n = nums.length;
+        if(n == 0) return new ArrayList<>();
+        if(n == 1)  {
+            return new ArrayList<>(){
+                {
+                    add(new ArrayList<>());
+                    add(new ArrayList<>(){
+                        {
+                            add(nums[0]);
+                        }
+                    });
+                }
+            };
+        }
+
+        Arrays.sort(nums);
+
+        List<List<Integer>> rst = new ArrayList<>();
+        rst.add(new ArrayList<>());
+        backTrace(rst,null,n,nums,n,0);
+
+
+        return rst;
+    }
+
+    public void backTrace(List<List<Integer>> rst, List<Integer> oneSubSet, int tk, int[] nums, int n ,int k  ){
+        if(tk == 0){
+            return;
+        }
+
+        for(int i = k; i < n  ;i++ ){
+            List<Integer> newSubSet = new ArrayList<>();
+            if(oneSubSet != null) newSubSet.addAll(oneSubSet);
+            newSubSet.add(nums[i]);
+            rst.add(newSubSet);
+
+            backTrace(rst, newSubSet, tk - 1,nums, n, i + 1);
+
+        }
+
+    }
+
+    // 组合
+    public List<List<Integer>> combine(int n, int k) {
+        if(k > n) return null;
+        if(k == n){
+            return new ArrayList<>(){
+                {
+                    add(new ArrayList<>(){
+                        {
+                            for (int i = 1; i <= n; i++) {
+                                add(i);
+                            }
+                        }
+                    });
+                }
+            };
+        }
+
+        List<List<Integer>> rst = new ArrayList<>();
+        backtrack(rst,null,0,1,n,k);
+
+        return rst;
+     }
+
+
+     public void backtrack(List<List<Integer>> rst, List<Integer> oneCombine,int count, int currentN, int n, int k){
+        if(count == k){
+            rst.add(oneCombine);
+            return;
+        }
+
+        for(int i = currentN; i <= n ;i++){
+            List<Integer> newCombine;
+            if(oneCombine == null) {
+               newCombine = new ArrayList<>();
+            }else{
+                newCombine = new ArrayList<>(oneCombine);
+            }
+
+            newCombine.add(i);
+            int newCount = count+1, newCurrentN = i + 1;
+            backtrack(rst,newCombine,newCount,newCurrentN,n,k);
+        }
+     }
+
+    // 搜索二维矩阵
+    public boolean searchMatrix2(int[][] matrix, int target){
+        boolean t = false;
+
+        int rows = matrix.length;
+        if(rows == 0) return false;
+        int columns = matrix[0].length;
+
+        int left = 0, right = rows * columns - 1;
+
+        while(left <= right){
+            int mid = left + (right - left) /2;
+            int m = mid / columns, n = mid % columns;
+            int midValue = matrix[m][n];
+            if(midValue == target) return true;
+            else if(midValue > target) right = mid - 1;
+            else left = mid + 1;
+        }
+
+        return t;
+    }
+
+
+    //两次 二分查找，做的复杂了，可以通过转换数组下标的方式来搞
+    public boolean searchMatrix(int[][] matrix, int target) {
+        boolean t = false;
+        int rows = matrix.length;
+        if(rows == 0) return false;
+
+        int column = matrix[0].length;
+        if(column == 0 ) return false;
+        int k = 0;
+
+        int left = 0;
+        int right = rows -1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            int midValue = matrix[mid][0];
+            if(midValue == target) return true;
+            else if(midValue > target ){
+                right = mid - 1;
+            }else{
+                if(matrix[mid][column-1] == target) return true;
+                else if(matrix[mid][column-1] > target){
+                    k = mid;
+                    break;
+                }else{
+                    left = mid + 1;
+                }
+            }
+        }
+
+        return binarySearch(matrix[k],target);
+    }
+
+    public boolean binarySearch(int a[], int target){
+        boolean t = false;
+
+        int left = 0;
+        int right = a.length -1;
+
+        while(left <= right){
+            int mid = left + (right - left) /2;
+            if(a[mid] == target) {
+                t = true;
+                break;
+            }
+            else if( a[mid] > target) right  = mid-1;
+            else left = mid + 1;
+        }
+
+
+        return t;
+    }
+    // 矩阵置零
+    public void setZeroes(int[][] matrix) {
+        HashSet<Integer> zeroColumns = new HashSet<>();
+
+        int rows = matrix.length;
+        if(rows == 0) return;
+        int columns = matrix[0].length;
+
+        for(int i = 0; i < rows;i++){
+            boolean isZero = false;
+            for(int j = 0; j < columns;j++){
+                if(matrix[i][j] == 0){
+                    isZero = true;
+                    zeroColumns.add(j);
+                }
+            }
+
+            if(isZero) matrix[i] = new int[columns];
+        }
+
+        for(int i : zeroColumns){
+            for(int j = 0; j < rows;j++){
+                matrix[j][i] = 0;
+            }
+        }
+
+    }
+
+    //简化路径
+    public String simplifyPath(String path) {
+        Stack<String> dirs = new Stack<>();
+        int index = 0;
+        while(index < path.length()){
+            if(path.charAt(index) == '/'){
+                while(index < path.length() && path.charAt(index) == '/'){
+                    index++;
+                }
+                if(dirs.empty()){
+                    dirs.push("/");
+                }
+            }else if(path.charAt(index) == '.'){
+                String dots = "";
+                while(index < path.length() && path.charAt(index) == '.'){
+                    dots = dots + path.charAt(index++);
+                }
+                if(index < path.length() && path.charAt(index) != '/'){
+                    StringBuilder dir = new StringBuilder();
+                    while(index < path.length() && path.charAt(index) != '/' && path.charAt(index) != '.'){
+                        dir.append(path.charAt(index++));
+                    }
+                    dirs.push(dots+dir.toString());
+                } else if(dots.length() == 1) continue;
+                else if(dots.length() == 2) dirs.pop();
+                else {
+                    dirs.push(dots);
+                }
+            }else {
+                StringBuilder dir = new StringBuilder();
+                while(index < path.length() && path.charAt(index) != '/' && path.charAt(index) != '.'){
+                    dir.append(path.charAt(index++));
+                }
+                dirs.push(dir.toString());
+            }
+
+        }
+
+        if(dirs.size() == 0) return "/";
+
+        StringBuilder standardPath = new StringBuilder();
+        for(String dir: dirs){
+            standardPath.append("/"+dir);
+        }
+
+
+        if(dirs.size() == 1) standardPath.delete(1,2);
+        else standardPath.delete(1,3);
+        return standardPath.toString();
+    }
+
+    // 最小路径和
+    // 动态规划速度更快
+    public int minPathSum2(int[][] grid){
+        int rst = 0,m = grid.length,n = grid[0].length;
+        if( m == 1 ) {
+            for(int i : grid[0]) rst += i;
+            return rst;
+        }
+        if(n == 1){
+            for(int i = 0; i < m;i++) rst += grid[i][0];
+            return rst;
+        }
+        for(int i = 1; i < m; i++ ) grid[i][0] += grid[i-1][0];
+        for(int i = 1;i < n; i++) grid[0][i] += grid[0][i-1];
+        for(int i = 1; i < m;i++) for(int j = 1; j < n;j++) grid[i][j] = grid[i-1][j] < grid[i][j-1] ? grid[i][j] + grid[i-1][j] : grid[i][j] + grid[i][j-1];
+        return grid[m-1][n-1];
+    }
+
+    //回溯算法 超出时间限制
+    public int minPathSum(int[][] grid) {
+        int rst = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+
+        if( m == 1 ) {
+            for(int i : grid[0])
+                rst += i;
+            return rst;
+        }
+
+        if(n == 1){
+            for(int i = 0; i < m;i++)
+                rst += grid[i][0];
+            return rst;
+        }
+
+        int[] min = {-1};
+        findMax(min,0,0,grid,0);
+
+        return min[0];
+    }
+
+
+    public void findMax(int[] min, int m, int n, int[][] grid, int cur){
+        if(m >= grid.length || n >= grid[0].length) return;
+        int newCur = cur + grid[m][n];
+        if(m == grid.length-1 && n == grid[0].length-1){
+            if(min[0] == -1) min[0] = newCur;
+            else min[0] = newCur < min[0] ? newCur : min[0];
+        }
+
+        findMax(min,m+1,n,grid,newCur);
+        findMax(min,m,n+1,grid,newCur);
+    }
+
+
+
+
+    // 不同路径2
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         //行数
         int m = obstacleGrid.length;
@@ -312,7 +824,11 @@ public class Solution0701 {
     public static void main(String[] args) {
 
         Solution0701 s = new Solution0701();
-        System.out.println(s.uniquePaths(1,9));
+        int[] t = {4,5,6,7,0,1,2};
+        System.out.println(s.search(t,3));
+
+
+
 
 
     }
